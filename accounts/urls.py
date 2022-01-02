8a159210674b31f601c6b355474a93bc32cc2e04
Hasssan Ahmed <ahmed.hassan.112.ha@gmail.com>
2022-01-02 18:45:51 +0500

    login complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,11 @@ from django.views.generic.base import TemplateView
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.sitemaps import views as sitemaps_views
 from django.views.decorators.cache import cache_page
-from .views import AccountCreateApi, TestView
+from .views import AccountApi, TestView, LoginAPI
 
 urlpatterns = [
     # Company details and verification stuff
-    path('', AccountCreateApi.as_view(), name='account'),
+    path('', AccountApi.as_view(), name='account'),
+    path('login', LoginAPI.as_view(), name='login'),
     path('test/', TestView.as_view(), name='test-view'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpRequest, JsonResponse
@@ -9,17 +10,17 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
 
 
-class AccountCreateApi(View):
+class AccountApi(View):
     def post(self, request: HttpRequest, *args, **kwargs):
         response = {
             'success': False
         }
         # ('username')
-        # ('email')
         # ('password1')
         # ('password2')
         firstName = request.POST.get('fname')
         lastName = request.POST.get('lname')
+        email = request.POST.get('email')
         signupForm = UserCreationForm(data=request.POST)
 
         if signupForm.is_valid():
@@ -30,12 +31,38 @@ class AccountCreateApi(View):
             user.is_active = True
             user.first_name = firstName
             user.last_name = lastName
+            user.email = email
             user.save()
             response['success'] = True
         else:
             response['success'] = False
             response['errors'] = signupForm.errors
 
+        return JsonResponse(response)
+
+
+class LoginAPI(View):
+    def post(self, request: HttpRequest, *args, **kwargs):
+        response = {
+            'authenticated': False,
+            'user_data': None
+        }
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user: User = authenticate(username=username, password=password)
+
+        if user is not None:
+            response['authenticated'] = True
+            response['user_data'] = {
+                'username': user.username,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'email': user.email,
+
+            }
+        else:
+            response['authenticated'] = False
+            response['user_data'] = None
         return JsonResponse(response)
 
 

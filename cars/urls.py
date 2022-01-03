585a50b87a037ea1911dfbae83cea77536d075ac
Hasssan Ahmed <ahmed.hassan.112.ha@gmail.com>
2022-01-03 18:40:14 +0500

    minor changes

--- cars/urls.py
+++ cars/urls.py
@@ -3,9 +3,10 @@ from django.views.generic.base import TemplateView
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.sitemaps import views as sitemaps_views
 from django.views.decorators.cache import cache_page
-from .views import  CarApi
+from .views import CarApi, DeleteCarApi
 
 urlpatterns = [
     # Company details and verification stuff
     path('', CarApi.as_view(), name='cars'),
+    path('delete/', DeleteCarApi.as_view(), name='delete-car'),
 ]
